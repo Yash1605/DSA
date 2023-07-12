9bd75a558d908974a6be7db4023afeb60ed28144
Yash1605 <yashshrivastava1605@gmail.com>
2023-07-12 16:34:10 +0530

    Level order traversal

--- src/BinaryTree/Level Order Traversal/Main.java
+++ src/BinaryTree/Level Order Traversal/Main.java
@@ -21,3 +21,42 @@ class Solution
         return res;
     }
 }
+/**https://leetcode.com/problems/binary-tree-level-order-traversal/
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>>res=new ArrayList<>();
+//          Queue<TreeNode>que=new ArrayDeque<>();
+//          if(root==null)return res;
+//          que.add(root);
+//         while(que.size()>0){
+            
+//             int n=que.size();
+//             List<Integer>level=new ArrayList<>();
+//            while(n-->0){
+//     TreeNode temp=que.remove();
+//     level.add(temp.val); // add the value of the current node to the level list
+//     if(temp.left!=null){
+//         que.add(temp.left);
+//     }
+//     if(temp.right!=null){
+//         que.add(temp.right);
+//     }
+// } res.add(level);
+//         }
+//         return res;
+//     }
+// }
